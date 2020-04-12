20dcca6b1cc2a52293418f48f33062424bc2f09d
elizabet38 <ubn[f,38ujdyj>
2020-04-12 16:41:00 +0300

    QwErTy

--- app/src/main/java/com/example/androidapplication/MainActivity.kt
+++ app/src/main/java/com/example/androidapplication/MainActivity.kt
@@ -1,14 +1,78 @@
 package com.example.androidapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
+import android.widget.TextView
 
 
-class MainActivity : AppCompatActivity() {
+/*class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+}*/
+
+class MainActivity : Activity(), View.OnTouchListener {
+
+    var tv: TextView? = null
+    var x = 0f
+    var y = 0f
+    var z = 0
+    var circleNumber: String? = null
+
+
+
+    /** Called when the activity is first created.  */
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tv = TextView(this)
+        tv!!.setOnTouchListener(this)
+        setContentView(tv)
+    }
+
+
+
+
+
+    override fun onTouch(v: View, event: MotionEvent): Boolean {
+        x = event.x
+        y = event.y
+        z++
+
+
+         fun createCircle(canvas: Canvas) {
+
+
+            val paint = Paint()
+            paint.setColor(Color.BLACK)
+
+            paint.setStyle(Paint.Style.FILL)
+            canvas.drawCircle(x, y, 10F, paint)
+
+        }
+        createCircle(canvas = Canvas())
+
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                circleNumber = "Number of Circles: $z"
+            }
+            MotionEvent.ACTION_MOVE -> circleNumber = "Number of Circles: $z"
+            MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                circleNumber = "Number of Circles: $z"
+            }
+        }
+        tv!!.text = """
+            $circleNumber
+            """.trimIndent()
+        return true
     }
 }
--- app/src/main/java/com/example/androidapplication/views/CustomView.kt
+++ app/src/main/java/com/example/androidapplication/views/CustomView.kt
@@ -1,9 +1,17 @@
 package com.example.androidapplication.views
 
+import android.app.Activity
 import android.content.Context
-import android.graphics.*
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
+import android.os.Bundle
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
+import android.view.View.OnTouchListener
+import android.widget.TextView
 
 
 class CustomView(context: Context, attrs: AttributeSet) : View(context, attrs) {
@@ -17,27 +25,29 @@ class CustomView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
 
 
-private fun createDrawingField(canvas: Canvas) {
+    private fun createDrawingField(canvas: Canvas) {
 
-    val width = width.toFloat()
-    val height = height.toFloat()
+        val width = width.toFloat()
+        val height = height.toFloat()
 
-    val paint = Paint()
-    paint.isAntiAlias = true
-    paint.color = Color.BLACK
-    paint.strokeWidth = 5f
-    paint.style = Paint.Style.FILL
+        val paint = Paint()
+        paint.isAntiAlias = true
+        paint.color = Color.BLACK
+        paint.strokeWidth = 5f
+        paint.style = Paint.Style.FILL
 
 
-    val rect = RectF()
-    rect[69F, 69F, width - 69F] = height - 69F
-    canvas.drawRoundRect(rect, 100F, 100F, paint)
-    paint.color = Color.WHITE
-    rect[75F, 75F, width - 75F] = height - 75F
-    canvas.drawRoundRect(rect, 100F, 100F, paint)
+        val rect = RectF()
+        rect[69F, 69F, width - 69F] = height - 69F
+        canvas.drawRoundRect(rect, 100F, 100F, paint)
+        paint.color = Color.WHITE
+        rect[75F, 75F, width - 75F] = height - 75F
+        canvas.drawRoundRect(rect, 100F, 100F, paint)
 
 
     }
+
+
 
 
 
