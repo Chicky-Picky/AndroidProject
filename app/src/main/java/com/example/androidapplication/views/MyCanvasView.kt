4db22f4cc6ac6ee5565bb7d706e0bee11ce8f923
elizabet38 <ubn[f,38ujdyj>
2020-05-14 19:25:01 +0300

    Added millions of fiches

--- app/src/main/java/com/example/androidapplication/views/MyCanvasView.kt
+++ app/src/main/java/com/example/androidapplication/views/MyCanvasView.kt
@@ -18,7 +18,6 @@ import kotlin.math.abs
 import kotlin.math.sqrt
 
 
-private const val STROKE_WIDTH = 38f
 class MyCanvasView(context: Context) : View(context) {
     private var listOfListOfPoints = ArrayList<ArrayList<Point>>()
     private var currentNumberOfPoints = 0
@@ -27,6 +26,9 @@ class MyCanvasView(context: Context) : View(context) {
     private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
     private var vector = VectorizationImpl()
     private var lineSegmentsEnds = ArrayList<Point>()
+    private var lineSegmentsEdges = ArrayList<ArrayList<Int>>()
+    private var lineSegmentIndexes1 = ArrayList<Int>()
+    private var lineSegmentIndexes2 = ArrayList<Int>()
     var points = Points(arrayListOf(), arrayListOf())
 
 
@@ -39,17 +41,27 @@ class MyCanvasView(context: Context) : View(context) {
         style = Paint.Style.STROKE
         strokeJoin = Paint.Join.ROUND
         strokeCap = Paint.Cap.ROUND
-        strokeWidth = 38f
+        strokeWidth = 10f
     }
 
-    private val paint2 = Paint().apply {
+    private val paint1 = Paint().apply {
         color = Color.RED
         isAntiAlias = true
         isDither = true
         style = Paint.Style.STROKE
         strokeJoin = Paint.Join.ROUND
         strokeCap = Paint.Cap.ROUND
-        strokeWidth = 19f
+        strokeWidth = 15f
+    }
+
+    private val paint2 = Paint().apply {
+        color = Color.YELLOW
+        isAntiAlias = true
+        isDither = true
+        style = Paint.Style.STROKE
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+        strokeWidth = 15f
     }
 
     private val paint3 = Paint().apply {
@@ -59,9 +71,18 @@ class MyCanvasView(context: Context) : View(context) {
         style = Paint.Style.STROKE
         strokeJoin = Paint.Join.ROUND
         strokeCap = Paint.Cap.ROUND
-        strokeWidth = 19f
+        strokeWidth = 15f
     }
 
+    private val paint4 = Paint().apply {
+        color = Color.BLUE
+        isAntiAlias = true
+        isDither = true
+        style = Paint.Style.STROKE
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+        strokeWidth = 15f
+    }
 
     private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
 
@@ -70,20 +91,23 @@ class MyCanvasView(context: Context) : View(context) {
     private var motionTouchEventY = 0f
 
 
-    fun dist(x: Point, y: Point): Double{
+    private fun dist(x: Point, y: Point): Double{
         val z = Point(x.x - y.x, x.y - y.y)
         return sqrt(z.x * z.x + z.y * z.y)
     }
 
 
-    fun distCheck (x: ArrayList<Point>): ArrayList<Point>{
-        var y: ArrayList<Point> = arrayListOf(Point(0.0, 0.0), Point(0.0, 0.0))
+    private fun distCheck (x: ArrayList<Point>): ArrayList<Point>{
+        val y: ArrayList<Point> = arrayListOf(Point(0.0, 0.0), Point(0.0, 0.0))
+        var i1 = -1
+        var i2 = -1
         var d = 138.0
         for (i in 0 until lineSegmentsEnds.size)
         {
             val z = lineSegmentsEnds[i]
             if (dist(x[0], z) < d)
             {
+                i1 = i
                 y[0] = z
                 d = dist(x[0], z)
             }
@@ -95,22 +119,112 @@ class MyCanvasView(context: Context) : View(context) {
             val z = lineSegmentsEnds[i]
             if (dist(x[1], z) < d)
             {
+                i2 = i
                 y[1] = z
                 d = dist(x[1], z)
             }
         }
         if (y[0] == Point(0.0, 0.0))
         {
+            lineSegmentsEdges.add(arrayListOf())
+            i1 = lineSegmentsEnds.size
             lineSegmentsEnds.add(x[0])
             y[0] = x[0]
         }
         if (y[1] == Point(0.0, 0.0))
         {
+            lineSegmentsEdges.add(arrayListOf())
+            i2 = lineSegmentsEnds.size
             lineSegmentsEnds.add(x[1])
             y[1] = x[1]
         }
+        lineSegmentIndexes1.add(i1)
+        lineSegmentIndexes2.add(i2)
+        lineSegmentsEdges[i1].add(i2)
+        lineSegmentsEdges[i2].add(i1)
         return y
     }
+
+
+    /*private fun rectCheck(A: Point, B: Point, C: Point, D: Point): ArrayList<Point>{
+        val ans : ArrayList<Point> = arrayListOf(C, D)
+        val a = dist(A, B)
+        val b = dist(B, C)
+        val c = dist(C, D)
+        val d = dist(D, A)
+        val ax = B.x - A.x
+        val ay = B.y - A.y
+        val bx = C.x - B.x
+        val by = C.y - B.y
+        val cx = D.x - C.x
+        val cy = D.y - C.y
+        val dx = A.x - D.x
+        val dy = A.y - D.y
+
+
+        val da = ax/ay
+        val db = bx/by
+        val dd = dx/dy
+        if (abs(b - d) > 50)
+        {
+            return ans
+        }
+        if (abs(a - c) > 100)
+        {
+            return ans
+        }
+        if (abs(da * db) < 0.75 || abs(da * db) > 1.38)
+        {
+            return ans
+        }
+        if (abs(da * dd) < 0.75 || abs(da * dd) > 1.38)
+        {
+            return ans
+        }
+        val dif = abs(da)
+        val dis = (b + c) / 2
+        var disty = sqrt(dis * dis / (dif * dif + 1))
+        var distx = dif * disty
+        if (ax > 0 && ay > 0)
+        {
+            disty *= -1
+            if (bx < 0)
+            {
+                distx *= -1
+                disty *= -1
+            }
+        }
+        if (ax > 0 && ay < 0)
+        {
+            distx *= -1
+            disty *= -1
+            if (bx > 0)
+            {
+                distx *= -1
+                disty *= -1
+            }
+        }
+        if (ax < 0 && ay < 0)
+        {
+            distx *= -1
+            if (bx > 0)
+            {
+                distx *= -1
+                disty *= -1
+            }
+        }
+        if (ax < 0 && ay > 0)
+        {
+            if (bx < 0)
+            {
+                distx *= -1
+                disty *= -1
+            }
+        }
+        ans[0] = Point(B.x + distx, B.y + disty)
+        ans[1] = Point(A.x + distx, A.y + disty)
+        return ans
+    }*/
 
 
 
@@ -147,6 +261,56 @@ class MyCanvasView(context: Context) : View(context) {
             {
                 canvas.drawLine(points.shapes[i][0].x.toFloat(), points.shapes[i][0].y.toFloat(), points.shapes[i][1].x.toFloat(), points.shapes[i][1].y.toFloat(), paint2)
 
+                /*var t = ArrayList<Point>()
+                t = distCheck(points.shapes[i])
+                val c = t[0]
+                val d = t[1]
+                var i1 = -1
+                var i2 = -1
+                var i3 = -1
+
+                if (points.shapes[i][0] != c && points.shapes[i][1] != d)
+                {
+                    for (j in 0 until lineSegmentsEnds.size)
+                    {
+                        if (c == lineSegmentsEnds[j])
+                        {
+                            i1 = j
+                        }
+                        if (d == lineSegmentsEnds[j])
+                        {
+                            i2 = j
+                        }
+                    }
+                    for (j in 0 until lineSegmentsEdges[i1].size)
+                    {
+                        for (k in 0 until lineSegmentsEdges[i2].size)
+                        {
+                            var a = lineSegmentsEnds[lineSegmentsEdges[i1][j]]
+                            var b = lineSegmentsEnds[lineSegmentsEdges[i2][k]]
+                            for (l in 0 until lineSegmentsEnds.size)
+                            {
+                                if (a == lineSegmentsEnds[l])
+                                {
+                                    i3 = l
+                                }
+                            }
+                            for (l in 0 until lineSegmentsEdges[i3].size)
+                            {
+                                if (b == lineSegmentsEnds[lineSegmentsEdges[i3][l]])
+                                {
+                                    var cd = rectCheck(a, b, d, c)
+                                    canvas.drawLine(a.x.toFloat(), a.y.toFloat(), b.x.toFloat(), b.y.toFloat(), paint4)
+                                    canvas.drawLine(b.x.toFloat(), b.y.toFloat(), cd[0].x.toFloat(), cd[0].y.toFloat(), paint4)
+                                    canvas.drawLine(cd[0].x.toFloat(), cd[0].y.toFloat(), cd[1].x.toFloat(), cd[1].y.toFloat(), paint4)
+                                    canvas.drawLine(cd[1].x.toFloat(), cd[1].y.toFloat(), a.x.toFloat(), a.y.toFloat(), paint4)
+                                }
+                            }
+                        }
+                    }
+
+                }*/
+
                 points.shapes[i] = distCheck(points.shapes[i])
 
                 canvas.drawLine(points.shapes[i][0].x.toFloat(), points.shapes[i][0].y.toFloat(), points.shapes[i][1].x.toFloat(), points.shapes[i][1].y.toFloat(), paint3)
@@ -155,7 +319,7 @@ class MyCanvasView(context: Context) : View(context) {
             }
             if (points.shapeType[i] == "ellipse")
             {
-                canvas.drawOval(points.shapes[i][0].x.toFloat(), points.shapes[i][1].y.toFloat(), points.shapes[i][2].x.toFloat(), points.shapes[i][3].y.toFloat(), paint2)
+                canvas.drawOval(points.shapes[i][0].x.toFloat(), points.shapes[i][1].y.toFloat(), points.shapes[i][2].x.toFloat(), points.shapes[i][3].y.toFloat(), paint1)
             }
         }
 
