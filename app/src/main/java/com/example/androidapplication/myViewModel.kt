1e6072facbd798da17b1ff931af9ee4ae1b1abee
Anastasia Kazovskaya <kazovskayan13@gmail.com>
2020-05-16 19:13:33 +0300

    Correcting commits

--- app/src/main/java/com/example/androidapplication/myViewModel.kt
+++ app/src/main/java/com/example/androidapplication/myViewModel.kt
@@ -3,7 +3,8 @@ package com.example.androidapplication
 import androidx.lifecycle.ViewModel
 import com.example.algo.Point
 import com.example.algo.Points
-
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 
 class MyViewModel : ViewModel() {
     private var listsOfPoints = ArrayList<ArrayList<Point>>()
@@ -12,6 +13,20 @@ class MyViewModel : ViewModel() {
     private var lineSegments = ArrayList<Point>()
     private var shapes = Points(arrayListOf(), arrayListOf())
     public var mode: Int = 0
+
+    var listOfListOfPoints : MutableLiveData<ArrayList<ArrayList<Point>>>? = null
+
+    /*
+    fun getListOfListOfPoints(): LiveData<ArrayList<ArrayList<Point>>> {
+        if (listOfListOfPoints == null) {
+            listOfListOfPoints = MutableLiveData()
+            loadListOfListOfPoints()
+        }
+    }
+
+    private fun loadListOfListOfPoints() {
+    }
+    */
 
     fun getLists(): ArrayList<ArrayList<Point>> {
         return listsOfPoints
